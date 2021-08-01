59b4160ccdaf06f0e2cbb29077309653c23ffc79
AnimatedSwine37 <alolroad@hotmail.com>
2021-08-01 16:39:26 +1000

    Copy files correctly on build and publish

--- p4gpc.rpc/Publish.ps1
+++ p4gpc.rpc/Publish.ps1
@@ -20,9 +20,17 @@ dotnet publish $solutionName -c Release -r win-x64 --self-contained false -o "$m
 
 # Remove Redundant Files
 Move-Item -Path "$modOutputPath/x86/ModConfig.json" -Destination "$modOutputPath/ModConfig.json"
+Move-Item -Path "$modOutputPath/x86/events.json" -Destination "$modOutputPath/events.json"
+Move-Item -Path "$modOutputPath/x86/fields.json" -Destination "$modOutputPath/fields.json"
 Move-Item -Path "$modOutputPath/x86/Preview.png" -Destination "$modOutputPath/Preview.png"
+Move-Item -Path "$modOutputPath/x86/discord_game_sdk.dll" -Destination "$modOutputPath/discord_game_sdk.dll"
+Move-Item -Path "$modOutputPath/x86/discord_game_sdk.dll.lib" -Destination "$modOutputPath/discord_game_sdk.dll.lib"
 Remove-Item "$modOutputPath/x64/Preview.png"
+Remove-Item "$modOutputPath/x64/discord_game_sdk.dll"
+Remove-Item "$modOutputPath/x64/discord_game_sdk.dll.lib"
 Remove-Item "$modOutputPath/x64/ModConfig.json"
+Remove-Item "$modOutputPath/x64/events.json"
+Remove-Item "$modOutputPath/x64/fields.json"
 
 # Cleanup Unnecessary Files
 Get-ChildItem $modOutputPath -Include *.exe -Recurse | Remove-Item -Force -Recurse
